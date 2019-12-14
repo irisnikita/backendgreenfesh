9737dbaf70199a0a9cc5c3324bb95a4c7f855564
Truong Vi <nltruongvi@gmail.com>
2019-12-14 22:21:21 +0700

    commit all

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ const mysql = require('mysql');
 
 const PORT = process.env.PORT || 6789;
 
-const app=express();
+const app = express();
 
 //connect to database
 const connMysql = mysql.createConnection({
     host: '35.225.6.209',
     user: 'truongvi1999',
     password: 'tjmwjm824594',
-    database: 'greenfeshdata'
+    database: 'greenfeshdata',
 });
 
-connMysql.connect(err=>{
-    (err)?console.log(err):console.log('Connect success !');
+connMysql.connect(err => {
+    err ? console.log(err) : console.log('Connect success !');
 });
 
-require('./routes/staffRouter')(app,connMysql);
+require('./routes/staffRouter')(app, connMysql);
 
-app.listen(PORT,()=>{
-    console.log(`App is running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
-
